a3f2ea04d6900e537e7c9e0f1d60fa9afd614ce5
lijasminej <li.jasmine.j@gmail.com>
2016-06-11 10:49:33 -0400

    Fix require

--- 0611-01-warmup/app/app.js
+++ 0611-01-warmup/app/app.js
@@ -1,7 +1,7 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var Parent = require('./components/Parent.js');
+var Parent = require('./components/Parent');
 
 ReactDOM.render(
   <Parent />, document.getElementById('app')
